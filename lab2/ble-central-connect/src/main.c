d4ed727e083765256df27f9c2d8a6036c0266c63
willbigger <wmb8yt@virginia.edu>
2023-03-15 11:23:48 -0400

    updates on notify function

--- lab2/ble-central-connect/src/main.c
+++ lab2/ble-central-connect/src/main.c
@@ -69,42 +69,20 @@
 
 //central
 #define LAB2_SERVICE_UUID BT_UUID_128_ENCODE(0xBDFC9792, 0x8234, 0x405E, 0xAE02, 0x35EF3274B299)
-#define LAB2_SERVICE_CHARACTERISTIC_UUID 0x0001
-#define LAB2_SERVICE_CHARACTERISTIC_UUID 0x0002
-#define LAB2_SERVICE_CHARACTERISTIC_UUID 0x0003
-#define LAB2_SERVICE_CHARACTERISTIC_UUID 0x0004
+#define LAB2_SERVICE_CHARACTERISTIC_UUID1 0x0001
+#define LAB2_SERVICE_CHARACTERISTIC_UUID2 0x0002
+#define LAB2_SERVICE_CHARACTERISTIC_UUID3 0x0003
+#define LAB2_SERVICE_CHARACTERISTIC_UUID4 0x0004
 
 static void start_scan(void);
 
 static struct bt_conn *default_conn;
 
 static struct bt_uuid* search_service_uuid = BT_UUID_DECLARE_128(LAB2_SERVICE_UUID);
-static struct bt_uuid* search_characteristic_uuid = BT_UUID_DECLARE_16(LAB2_SERVICE_CHARACTERISTIC_UUID);
+static struct bt_uuid* search_characteristic_uuid = BT_UUID_DECLARE_16(LAB2_SERVICE_CHARACTERISTIC_UUID1);
 static struct bt_gatt_discover_params discover_params;
 static struct bt_gatt_read_params read_params;
-
-// Callback after reading characteristic value.
-static uint8_t read_func(struct bt_conn *conn, uint8_t err,
-			       struct bt_gatt_read_params *params,
-			       const void *data, uint16_t length)
-{
-	if (err) {
-		printk("read failed (err %d)\n", err);
-	}
-
-	uint8_t* buf = (uint8_t*) data;
-
-	if (length == 4) {
-		uint32_t val = (((uint32_t) buf[0]) << 24) |
-					   (((uint32_t) buf[1]) << 16) |
-					   (((uint32_t) buf[2]) <<  8) |
-					   (((uint32_t) buf[3]) <<  0);
-
-		printk("Read: 0x%x\n", val);
-	}
-
-	return BT_GATT_ITER_STOP;
-}
+static struct bt_gatt_notify_params notify_params;
 
 // Callback after receiving a notification.
 static uint8_t notif_func(struct bt_conn *conn, uint8_t err,
@@ -114,6 +92,8 @@ static uint8_t notif_func(struct bt_conn *conn, uint8_t err,
 	const struct device *dev;
 	bool led_is_on = true;
 	int ret;
+
+	printk("Enering notify function");
 
 	if (err) {
 		printk("notify failed (err %d)\n", err);
@@ -160,16 +140,18 @@ static uint8_t discover_func(struct bt_conn *conn,
 			printk("Discover failed (err %d)\n", err);
 		}
 	}
-	else if (bt_uuid_cmp(discover_params.uuid, BT_UUID_DECLARE_16(LAB2_SERVICE_CHARACTERISTIC_UUID)) == 0) {
+	else if (
+		(bt_uuid_cmp(discover_params.uuid, BT_UUID_DECLARE_16(LAB2_SERVICE_CHARACTERISTIC_UUID1)) == 0) ||
+		(bt_uuid_cmp(discover_params.uuid, BT_UUID_DECLARE_16(LAB2_SERVICE_CHARACTERISTIC_UUID2)) == 0) ||
+		(bt_uuid_cmp(discover_params.uuid, BT_UUID_DECLARE_16(LAB2_SERVICE_CHARACTERISTIC_UUID3)) == 0) ||
+		(bt_uuid_cmp(discover_params.uuid, BT_UUID_DECLARE_16(LAB2_SERVICE_CHARACTERISTIC_UUID4)) == 0)
+	) {
 		// printk("Found characteristic\n");
-
-		read_params.func = read_func;
-		read_params.handle_count = 1;
-		read_params.single.handle = bt_gatt_attr_value_handle(attr);
-		read_params.single.offset = 0U;
+		notify_params.func = notif_func;
+		notify_params.attr = attr;
 
 		//err = bt_gatt_read(conn, &read_params);
-		err = bt_gatt_notify_cb(conn, &read_params);
+		err = bt_gatt_notify_cb(conn, &notify_params);
 		if (err) {
 			printk("Read failed (err %d)\n", err);
 		}
