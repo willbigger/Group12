ca4f395ba20f8c7b75f88c95bbcf47d0490c201c
Jiafu Li <jl8jp@virginia.edu>
2023-05-04 13:20:56 -0400

    send 200 bytes

--- Final-Project/Sender/user_app.py
+++ Final-Project/Sender/user_app.py
@@ -5,11 +5,19 @@ arduino = serial.Serial(port='/dev/cu.usbserial-0001', baudrate=115200, timeout=
 
 def write_read(x):
     arduino.write(bytes(x))
-    time.sleep(0.05)
+    time.sleep(0.25)
 while True:
     with open ('EncoderOutput.npy', 'rb') as file:
         input("Send?\n") # Taking input from user
-        write_read(file.read().strip())
+        while True:
+            piecesOfText = file.read(200)
+            if piecesOfText == '':
+                break
+            piecesOfText = piecesOfText + bytes("]")
+            write_read(piecesOfText)                
+        
+        #write_read(file.read(256))
+
     board_output = "."
     while board_output != "":
         board_output = arduino.readline().decode().strip()
